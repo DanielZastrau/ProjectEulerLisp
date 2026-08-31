2d1683792e0183adb02fdfa0aaa6d2e92f889bfb
DanielZastrau <daniel.zastrau1@gmx.de>
2026-08-31 19:51:27 +0200

    solved problem 3

---
+++ Problem3_LargestPrimeFactor.lisp
@@ -0,0 +1,18 @@
+(load "commons.lisp")
+
+(defun find-largest-prime-factor (n)
+    (declare (type fixnum n))
+    (let ((primes (commons:eratosthenes (truncate (sqrt n))))  (maximum 0)  (current n))
+        (declare (type fixnum current maximum) (type list primes)) 
+        (loop for prime of-type fixnum in primes
+            while (> current 1)
+            when (zerop (mod current prime))
+                do  (setf maximum prime)
+                    (loop while (zerop (mod current prime)) do (setf current (the fixnum (/ current prime)) ) ) )
+        (if (= current 1) maximum current) ) )
+
+(defun main () (print "Began") (finish-output) (time (print (find-largest-prime-factor 600851475143))))
+;  okay here we get to the problem that is mentioned in the docstring of the eratosthenes function
+;  technically 
+
+(main)
