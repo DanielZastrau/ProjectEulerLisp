8d222e093a158e1cd914693b49acd2bdd4dc1d5a
DanielZastrau <daniel.zastrau1@gmx.de>
2026-09-01 21:36:54 +0200

    solved problem 5

---
+++ Problem5_SmallestMultiple.lisp
@@ -0,0 +1,11 @@
+(load "commons.lisp")
+
+(defun main (limit) (declare (type fixnum limit))
+    (let ((primes (commons:eratosthenes limit))  (powers '())) (declare (type list primes))
+        (loop for prime in primes do (push (floor (/ (log limit 10) (log prime 10))) powers) )
+        
+        (let ((n 1)) (declare (type fixnum n))
+            (loop for prime in primes for power in (reverse powers) do (setf n (* n (expt prime power))) )
+            n) ) )
+
+(print "Began") (finish-output) (time (print (main 20)))
