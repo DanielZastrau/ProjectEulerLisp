ae11ebd8ca64ce069095b72fc31a2df0e4e47e5c
DanielZastrau <daniel.zastrau1@gmx.de>
2026-09-19 11:18:59 +0200

    solved problem 31

---
+++ Problem31_CoinSums.lisp
@@ -0,0 +1,11 @@
+(defun bottom-up (target coins)
+    (let ((ways (make-array (+ 1 target) :initial-element 0)))
+    (incf (aref ways 0))
+    (dolist (coin coins)
+        (do ((amount coin (+ 1 amount))) ((= amount (+ 1 target)))
+            (incf (aref ways amount) (aref ways (- amount coin)))))
+    (aref ways target)))
+(defun main ()
+    (bottom-up 200 (list 1 2 5 10 20 50 100 200)))
+
+(defun execute () (print "Began") (terpri) (time (print (main))))
